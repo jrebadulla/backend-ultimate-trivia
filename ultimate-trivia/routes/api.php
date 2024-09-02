e8edaeeb0ab1e6194315a5d43e04aa3ec528dbd0
JRebadulla123 <jellianrebadulla@gmail.com>
2024-09-02 20:14:12 +0800

    Add routes for new request

--- ultimate-trivia/routes/api.php
+++ ultimate-trivia/routes/api.php
@@ -1,6 +1,23 @@
 <?php
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\LevelController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\QuizController;
+use App\Http\Controllers\UserGameProgressController;
 
 Route::get('/getUsers', [LoginController::class, 'getUsers']);
 Route::post('/insertUsers', [LoginController::class, 'insertUsers']);
 Route::post('/userLogin', [LoginController::class, 'login']);
+
+
+Route::resource('levels', LevelController::class);
+Route::resource('questions', QuestionController::class);
+Route::resource('games', GameController::class);
+
+Route::resource('user_game_progress', UserGameProgressController::class);
+
+Route::post('/store', [QuestionController::class,'store']);
+
+Route::get('/quiz-questions', [QuizController::class, 'getQuizQuestions']);
+Route::post('/{gameId}/add-question', [QuizController::class, 'addQuestionToGame']);
--- ultimate-trivia/routes/web.php
+++ ultimate-trivia/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\GameController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\UserGameProgressController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LevelController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('levels', LevelController::class);
+Route::resource('questions', QuestionController::class);
+Route::resource('games', GameController::class);
+Route::resource('user_game_progress', UserGameProgressController::class);
+
