8dfa2e61335d914a59017047097b53f976e88636
JRebadulla123 <jellianrebadulla@gmail.com>
2024-09-02 20:27:22 +0800

    Set up model for users

---
+++ ultimate-trivia/app/Models/Users.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users';
+    protected $primaryKey = 'user_id';
+    public $incrementing = true;
+    protected $keyType = 'int';
+    protected $fillable = ['firstName', 'lastName', 'address', 'email', 'password', 'username'];
+    public $timestamps = true;
+}
