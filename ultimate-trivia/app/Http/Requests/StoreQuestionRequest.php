5960f862cdd003b112e617a0bbac576fc8aa0020
JRebadulla123 <jellianrebadulla@gmail.com>
2024-09-02 20:29:54 +0800

    Create Requests rule for question input

---
+++ ultimate-trivia/app/Http/Requests/StoreQuestionRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreQuestionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // Change to true to allow all users to make this request
+        // You can add your own logic if you need specific authorization
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'game_id' => 'required|string',
+            'level_id' => 'required|string',
+            'question_text' => 'required|string|max:255',
+            'correct_answer' => 'required|string|max:255',
+            'option_a' => 'nullable|string|max:255',
+            'option_b' => 'nullable|string|max:255',
+            'option_c' => 'required|string|max:255',
+            'option_d' => 'required|string|max:255',
+        ];
+    }
+}
+
